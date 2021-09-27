c475de3bfbd6fdd0fff8155b16c5de15d4d21fd4
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-09-27 23:03:56 +0530

    UC4 has been created where employee payroll is written into file and tested

--- src/com/employeepayroll/EmployeePayroll.java
+++ src/com/employeepayroll/EmployeePayroll.java
@@ -10,6 +10,36 @@ public class EmployeePayroll {
 		this.name = name;
 		this.salary = salary;
 	}
+	
+	public int getEmpId() {
+		return empId;
+	}
+
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public double getSalary() {
+		return salary;
+	}
+
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
 
 	@Override
 	public String toString() {
